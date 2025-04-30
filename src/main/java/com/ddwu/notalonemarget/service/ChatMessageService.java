42109b309382ce487b82420703e9c84583f83b41
seojin-kim12 <80455266+seojin-kim12@users.noreply.github.com>
2025-05-01 01:23:14 +0900

    Delete src/main/java/com/ddwu/notalonemarget directory

--- src/main/java/com/ddwu/notalonemarget/service/ChatMessageService.java
+++
@@ -1,7 +0,0 @@
-package com.ddwu.notalonemarget.service;
-
-import com.ddwu.notalonemarget.dto.ChatMessageDTO;
-
-public interface ChatMessageService {
-    void sendMessage(ChatMessageDTO message);
-}
