24f9c8f4e02828c672d99e3132edf501612dd445
Juhee <pjh402941@gmail.com>
2025-04-30 13:18:15 +0900

    � Initial Spring Boot project setup

---
+++ src/main/java/com/ddwu/notalonemarget/NotalonemargetApplication.java
@@ -0,0 +1,13 @@
+package com.ddwu.notalonemarget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotalonemargetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotalonemargetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddwu/notalonemarget/NotalonemargetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddwu.notalonemarget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotalonemargetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
