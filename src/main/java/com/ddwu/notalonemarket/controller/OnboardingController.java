3a4d371f9d7cd284ae5047354e16ccab5a711d11
hyunseo <ols3040015@naver.com>
2025-06-22 05:40:29 +0900

    feat: Thymeleaf

---
+++ src/main/java/com/ddwu/notalonemarket/controller/OnboardingController.java
@@ -0,0 +1,13 @@
+package com.ddwu.notalonemarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OnboardingController {
+
+    @GetMapping("/onboarding")
+    public String onboardingPage() {
+        return "onboarding"; // → templates/onboarding.html
+    }
+}
